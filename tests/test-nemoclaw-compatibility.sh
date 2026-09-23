ea9a5019e081ced3eb6376b938ace5de6ef7cafb
Linoy Bitan <lbitan@redhat.com>
2026-09-23 10:47:03 +0300

    fix: use native OpenShell binaries on VM PATH

--- tests/test-nemoclaw-compatibility.sh
+++ tests/test-nemoclaw-compatibility.sh
@@ -15,6 +15,9 @@ saw_render="$(helm template openshell-saw "$repo_root/charts/openshell-saw" \
 # wrappers would change the executable identity NemoClaw uses for attachment.
 grep -q 'openshell CLI upgraded from image' <<<"$saw_render"
 ! grep -q 'openshell-supervisor-wrapper' <<<"$saw_render"
+! grep -q 'openshell version wrapper installed' <<<"$saw_render"
+grep -q 'install -m 755 /usr/local/bin/openshell-gateway' <<<"$saw_render"
+grep -q 'install -m 755 /usr/local/bin/openshell /home' <<<"$saw_render"
 
 policy_render="$(helm template governance-policy "$repo_root/charts/governance-policy" \
   --namespace openshell-agents)"
